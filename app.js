f7232e1c966a04ba5f6e6e566a07acc8bbce0374
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-06-19 17:59:13 -0700

    get and post endpoints for CustomerInfo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const swaggerSpec = require('./swagger/swaggerConfig');
+const swaggerSpec = require('./swaggerConfig');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/ui', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/CustomerInfo', (req, res) => {
+    res.send('Customer Info goes here');
+});
+
+app.post('/CustomerInfo', (req, res) => {
+    console.log(req.body);
+    res.json({ success: true, message: 'item created' });
+});
 
 app.listen(8080, (err) => {
     if (err) {
